2f5b523cde321307b0be88213f669ab1c3614a85
William Best <wmbest2@gmail.com>
2014-04-23 19:33:27 -0500

    Go Gettable server

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"encoding/json"
 	"github.com/go-martini/martini"
-	"github.com/wmbest2/android/adb"
+	"github.com/wmbest2/rats_server/rats"
 	"net/http"
+    "runtime"
+    "path/filepath"
 )
 
 func GetDevices(parms martini.Params) (int, string) {
-	deviceLock.Lock()
-	b, _ := json.Marshal(devices)
-	deviceLock.Unlock()
+	rats.DeviceLock.Lock()
+	b, _ := json.Marshal(rats.Devices)
+	rats.DeviceLock.Unlock()
 	return http.StatusOK, string(b)
 }
 
+func serveStatic(m *martini.Martini) {
+    _, file, _, _ := runtime.Caller(0)
+	here := filepath.Dir(file)
+    static := filepath.Join(here, "/public")
+    m.Use(martini.Static(string(static))) 
+}
+
 func main() {
-	go updateAdb(5)
+	go rats.UpdateAdb(5)
 
 	m := martini.Classic()
 	r := martini.NewRouter()
